e0d5f1654d700d1039b2441c2e4d180826f8b76c
Dan Seagren <seagrendaniel562@gmail.com>
2020-10-01 15:44:17 -0700

    fix query bus issue

--- comments/index.js
+++ comments/index.js
@@ -1,27 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { randomBytes } = require('crypto')
-const cors = require('cors')
-const axios = require('axios')
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const commentsByPostId = {}
+const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-  res.send(commentsByPostId[req.params.id] || [])
-})
+  res.send(commentsByPostId[req.params.id] || []);
+});
 
 app.post('/posts/:id/comments', async (req, res) => {
-  const commentId = randomBytes(4).toString('hex')
-  const { content } = req.body
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-  const comments = commentsByPostId[req.params.id] || []
+  const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content })
-  commentsByPostId[req.params.id] = comments
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
 
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
@@ -30,17 +31,17 @@ app.post('/posts/:id/comments', async (req, res) => {
       content,
       postId: req.params.id
     }
-  })
+  });
 
-  res.status(201).send(comments)
-})
+  res.status(201).send(comments);
+});
 
 app.post('/events', (req, res) => {
-  console.log('Received Event', req.body.type)
-  res.send({})
-})
+  console.log('Event Received:', req.body.type);
+
+  res.send({});
+});
 
 app.listen(4001, () => {
-  console.log('Listening on 4001')
-})
-
+  console.log('Listening on 4001');
+});
